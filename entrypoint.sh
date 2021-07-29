1e96d845e736f3f5355202e9fd720344603e3aae
Dvin <dvyu@yandex.ru>
2021-07-29 16:41:03 +0500

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Cloning git repository"
 git config --global user.name "${GIT_USER}"
 git config --global user.email "${GIT_EMAIL}"
 mkdir /tmp/git
-git clone --single-branch --branch "$GIT_BRANCH" "https://$GIT_USER:$GIT_USER_API_TOKEN@github.com/$GIT_REPOSITORY_USERNAME/$GIT_REPOSITORY_NAME.git" /tmp/git
+git clone --single-branch --branch "$GIT_BRANCH" "https://x-access-token:$GIT_USER_API_TOKEN@github.com/$GIT_REPOSITORY_OWNER/$GIT_REPOSITORY_NAME.git" /tmp/git
 cd /tmp/git
 echo Test > testfile
 git add -A
