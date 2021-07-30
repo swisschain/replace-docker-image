f9f5d60e9354dcf82eb1e6d55829c051129de573
Dvin <dvyu@yandex.ru>
2021-07-30 12:27:43 +0500

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,20 @@ git config --global user.email "${GIT_EMAIL}"
 mkdir /tmp/git
 git clone --single-branch --branch "$GIT_BRANCH" "https://x-access-token:$GIT_USER_API_TOKEN@github.com/$GIT_REPOSITORY_OWNER/$GIT_REPOSITORY_NAME.git" /tmp/git
 cd /tmp/git
-echo Test > testfile
-git add -A
-git diff --cached
+#
 TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\/${INPUT_TAG_NAME_SKIP}//")
-git commit -m "Test commit ${TAG}"
-git push
-git log -2
+DOCKER_IMAGE=$(printf "%s/%s" $DOCKER_REPOSITORY_NAME $DOCKER_IMAGE_NAME)
+echo DOCKER_IMAGE=$DOCKER_IMAGE
+#
+for i in $(grep -rn $DOCKER_IMAGE: ./ | awk -F: '{print $1}')
+do
+  echo Processing $i
+  sed -E "s/${DOCKER_IMAGE}:.+$/${DOCKER_IMAGE}:${TAG}/" $i 
+  #mv $i.tmp $i
+done
+#
+#git add -A
+#git diff --cached
+#git commit -m "Test commit ${TAG}"
+#git push
+#git log -2
