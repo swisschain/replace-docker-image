872e13217dcc15a8791cdd3c89abbca58216a59f
Dvin <dvyu@yandex.ru>
2021-08-02 17:36:58 +0500

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e  # stops execution
 set -u  # undefined variable
 
 echo "Cloning git repository"
-git config --global user.name "${GIT_USER}"
-git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}"
 mkdir /tmp/git
-git clone --single-branch --branch "$GIT_BRANCH" "https://x-access-token:$GIT_USER_API_TOKEN@github.com/$GIT_INFRASTRUCTURE_REPOSITORY_OWNER/$GIT_INFRASTRUCTURE_REPOSITORY_NAME.git" /tmp/git
+git clone --single-branch --branch "$GIT_INFRASTRUCTURE_REPOSITORY_BRANCH" "https://x-access-token:$GIT_USER_API_TOKEN@github.com/$GIT_INFRASTRUCTURE_REPOSITORY_OWNER/$GIT_INFRASTRUCTURE_REPOSITORY_NAME.git" /tmp/git
 cd /tmp/git
 #
 TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\/${INPUT_TAG_NAME_SKIP}//")
